aeb7257c2dccaa04dae6c3f3d59b373420f55f5c
Gopinath Rajamanickam <gopinathrajamanickam@users.noreply.github.com>
2020-11-17 06:47:26 -0800

    Update SinglyLinkedList.py

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -144,6 +144,28 @@ class SinglyLinkedList:
             final_print += " -> None"
             print(final_print)
 
+    def find_mid(self):
+        if lst.is_empty():
+            return -1
+        current_node = lst.get_head()
+        if current_node.next_element == None:
+            # Only 1 element exist in array so return its value.
+            return current_node.data
+
+        mid_node = current_node
+        current_node = current_node.next_element.next_element
+        # Move mid_node (Slower) one step at a time
+        # Move current_node (Faster) two steps at a time
+        # When current_node reaches at end, mid_node will be at the middle of List
+        while current_node:
+            mid_node = mid_node.next_element
+            current_node = current_node.next_element
+        if current_node:
+            current_node = current_node.next_element
+        if mid_node:
+            return mid_node.data
+        return -1
+
 
 lst = SinglyLinkedList()
 lst.insert_at_head(1)
@@ -152,6 +174,7 @@ lst.insert_at_tail(3)
 lst.insert_at_tail(4)
 lst.insert_at_pos(5, 3)
 lst.print_list()
+print('Middle Element is {0}'.format(lst.find_mid()))
 lst.search(3)
 lst.delete_at_head()
 lst.print_list()
